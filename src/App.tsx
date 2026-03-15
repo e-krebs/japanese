a70e963079b58614190c9d9c9b2c2cc9bb86cb30
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2026-03-15 22:14:26 +0100

    feat: add title + top toolbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Copy, X } from "lucide-react";
 import { useReducer } from "react";
 
 import { Kana, Toolbar } from "./components";
 import { hiraganas as allHiraganas, katakanas as allKatakanas, type SyllabaryType } from "./data";
 import "./index.css";
 import "@fontsource/noto-sans-jp/400.css";
-import { useKanaProps } from "./hooks";
+import { useKanaProps, useSelectedKanas } from "./hooks";
+import { twMerge } from "tailwind-merge";
 
 interface State {
   type: SyllabaryType;
@@ -17,6 +19,11 @@ interface Action {
 
 export const App = () => {
   const { showDiacritics, showPalatalizers, toggleDiacritics, togglePalatalizers } = useKanaProps();
+  const { kanas, clearKanas } = useSelectedKanas();
+  const [shownType, toggleShownType] = useReducer<"kana" | "romaji", []>(
+    (old) => (old === "kana" ? "romaji" : "kana"),
+    "kana",
+  );
 
   const [{ type, allKanas }, dispatch] = useReducer<State, [Action]>(
     (state, action) => {
@@ -34,11 +41,46 @@ export const App = () => {
 
   return (
     <div className="relative p-4 pb-24 min-h-screen max-w-fit mx-auto flex flex-col items-center">
+      <h1 className="text-3xl pt-4 pb-7 capitalize">{type}s</h1>
+
+      {kanas.length > 0 && (
+        <Toolbar position="top">
+          <Toolbar.ButtonGroup>
+            <Toolbar.Button
+              onClick={() =>
+                navigator.clipboard.writeText(
+                  kanas.map((kana) => (shownType === "kana" ? kana.char : kana.romaji)).join(""),
+                )
+              }
+            >
+              <Copy className="size-4" />
+            </Toolbar.Button>
+            <Toolbar.Button
+              className="flex flex-col w-auto px-3 py-1 gap-y-2 overflow-hidden"
+              onClick={toggleShownType}
+            >
+              <div
+                className={twMerge(
+                  "text-2xl transition-[margin] duration-75",
+                  shownType === "kana" ? "mt-8" : "-mt-10",
+                )}
+              >
+                {kanas.map((kana) => kana.char).join("")}
+              </div>
+              <div className={twMerge("text-2xl")}>{kanas.map((kana) => kana.romaji).join("")}</div>
+            </Toolbar.Button>
+            <Toolbar.Button onClick={clearKanas}>
+              <X className="size-4" />
+            </Toolbar.Button>
+          </Toolbar.ButtonGroup>
+        </Toolbar>
+      )}
+
       <div className="flex flex-col items-center gap-5">
         {allKanas.map((kanasRow, index) => (
           <div key={index} className="flex gap-2.5">
             {kanasRow.map((kana) => (
-              <div key={kana.char}>
+              <div key={kana.char} className="h-fit">
                 <Kana kana={kana} />
               </div>
             ))}
