900951b72884b176349a12356c14a862d7ebe4db
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2026-03-15 14:36:22 +0100

    feat: button group, toggle button

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,34 +1,63 @@
 import { type ReactNode } from "react";
 import { twMerge } from "tailwind-merge";
 
+const ButtonGroup = ({ children }: { children: ReactNode }) => (
+  <div className="flex items-center justify-center gap-0.5 *:first:rounded-l-[2.5rem] *:last:rounded-r-[2.5rem]">
+    {children}
+  </div>
+);
+
 interface ButtonProps {
   selected?: boolean;
   children: ReactNode;
   onClick: () => void;
 }
 
-const Button = ({ selected, children, onClick }: ButtonProps) => {
-  return (
-    <button
-      className={twMerge("rounded-full size-12", selected && "bg-slate-400")}
-      disabled={selected}
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  );
-};
+const Button = ({ selected, children, onClick }: ButtonProps) => (
+  <button
+    className={twMerge(
+      "size-10 transition-all duration-75",
+      selected ? "bg-rose-200 text-slate-800 rounded-full!" : "bg-rose-900 rounded-xl",
+    )}
+    disabled={selected}
+    onClick={onClick}
+  >
+    {children}
+  </button>
+);
+
+interface ToggleButtonProps {
+  title: string;
+  children: ReactNode;
+  toggled: boolean;
+  onToggle: () => void;
+  className?: string;
+}
+
+const ToggleButton = ({ title, toggled, children, onToggle, className }: ToggleButtonProps) => (
+  <button
+    className={twMerge(
+      "size-10 flex items-center justify-center p-2 transition-all duration-75 overflow-hidden",
+      toggled ? "rounded-xl bg-rose-200 text-slate-800" : "rounded-full bg-rose-900",
+      className,
+    )}
+    onClick={onToggle}
+    title={title}
+  >
+    {children}
+  </button>
+);
 
 interface ToolbarProps {
   children: ReactNode;
 }
 
-export const Toolbar = ({ children }: ToolbarProps) => {
-  return (
-    <div className="fixed bottom-4 p-2 rounded-full flex items-center justify-center gap-1 bg-slate-500">
-      {children}
-    </div>
-  );
-};
+export const Toolbar = ({ children }: ToolbarProps) => (
+  <div className="fixed bottom-4 p-3 rounded-full flex items-center justify-center gap-2 bg-rose-800 shadow-md shadow-slate-900">
+    {children}
+  </div>
+);
 
+Toolbar.ButtonGroup = ButtonGroup;
 Toolbar.Button = Button;
+Toolbar.ToggleButton = ToggleButton;
