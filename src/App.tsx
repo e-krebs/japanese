952c4d51e4fca853dfb768dbca25e2ce68e755ae
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2026-03-15 22:13:53 +0100

    feat: upgrade toolbar

--- src/App.tsx
+++ src/App.tsx
@@ -46,7 +46,7 @@ export const App = () => {
         ))}
       </div>
 
-      <Toolbar>
+      <Toolbar color="vibrant">
         <Toolbar.ButtonGroup>
           <Toolbar.Button
             selected={type === "hiragana"}
--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -2,7 +2,7 @@ import { type ReactNode } from "react";
 import { twMerge } from "tailwind-merge";
 
 const ButtonGroup = ({ children }: { children: ReactNode }) => (
-  <div className="flex items-center justify-center gap-0.5 *:first:rounded-l-[2.5rem] *:last:rounded-r-[2.5rem]">
+  <div className="flex items-center justify-center gap-0.5 *:first:rounded-l-[2.5rem] *:first:rounded-r-2xl *:rounded-lg *:last:rounded-r-[2.5rem] *:last:rounded-l-2xl">
     {children}
   </div>
 );
@@ -11,13 +11,17 @@ interface ButtonProps {
   selected?: boolean;
   children: ReactNode;
   onClick: () => void;
+  className?: string;
 }
 
-const Button = ({ selected, children, onClick }: ButtonProps) => (
+const Button = ({ selected, children, onClick, className }: ButtonProps) => (
   <button
     className={twMerge(
-      "size-10 transition-all duration-75",
-      selected ? "bg-rose-200 text-slate-800 rounded-full!" : "bg-rose-900 rounded-xl",
+      "size-10 flex items-center justify-center rounded-xl transition-[border-radius] duration-150",
+      selected
+        ? "color-vibrant:bg-rose-200 color-standard:bg-slate-200 text-slate-800 rounded-full!"
+        : "color-vibrant:hover:bg-rose-900 color-standard:hover:bg-slate-700 active:color-vibrant:bg-rose-200 active:color-standard:bg-slate-200 active:text-slate-800",
+      className,
     )}
     disabled={selected}
     onClick={onClick}
@@ -37,8 +41,10 @@ interface ToggleButtonProps {
 const ToggleButton = ({ title, toggled, children, onToggle, className }: ToggleButtonProps) => (
   <button
     className={twMerge(
-      "size-10 flex items-center justify-center p-2 transition-all duration-75 overflow-hidden",
-      toggled ? "rounded-xl bg-rose-200 text-slate-800" : "rounded-full bg-rose-900",
+      "size-10 rounded-full flex items-center justify-center p-2 transition-all duration-75 overflow-hidden",
+      toggled
+        ? "color-vibrant:bg-rose-200 color-standard:bg-slate-200 text-slate-800"
+        : "color-vibrant:hover:bg-rose-900 color-standard:hover:bg-slate-700",
       className,
     )}
     onClick={onToggle}
@@ -50,10 +56,19 @@ const ToggleButton = ({ title, toggled, children, onToggle, className }: ToggleB
 
 interface ToolbarProps {
   children: ReactNode;
+  position?: "top" | "bottom";
+  color?: "standard" | "vibrant";
 }
 
-export const Toolbar = ({ children }: ToolbarProps) => (
-  <div className="fixed bottom-4 p-3 rounded-full flex items-center justify-center gap-2 bg-rose-800 shadow-md shadow-slate-900">
+export const Toolbar = ({ children, position = "bottom", color = "standard" }: ToolbarProps) => (
+  <div
+    data-color={color}
+    className={twMerge(
+      "fixed p-3 rounded-full flex items-center justify-center gap-2 shadow-md shadow-slate-900",
+      "data-[color=vibrant]:bg-rose-800 data-[color=standard]:bg-slate-800",
+      position === "top" ? "top-4" : "bottom-4",
+    )}
+  >
     {children}
   </div>
 );
